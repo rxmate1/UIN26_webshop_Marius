638963723de3dd60c5188119fe38b40e016dd0f2
rxmate1 <Marius.w.larsen2@gmail.com>
2026-01-19 12:45:04 +0100

    ...

--- LEGOdudes/shopscript.js
+++ LEGOdudes/shopscript.js
@@ -22,6 +22,65 @@ function fetchProducts() {
 
 fetchProducts()
 
+//Generer handlevogn
+function showCart(){
+
+    //Unike produkter
+    let uniqueItems = new Set(cart)
+    let uniqueArray = [...uniqueItems]
+    //Oversikt over antall per produkt
+    let cartItems =  []
+    uniqueArray.map(item => {
+        cartItems.push({prodid: item, quantity: cart.filter(i => i === item).length})
+    })
+    
+    //Gå gjennom cartItems for å lage HTML til handlekurven og regne ut totalpris
+    
+    let cartHTML = ""
+    let totalPrice = 0
+
+    cartItems.map(ci => {
+
+        //Hente produktinformasjon
+        let product = products.find(i => i.prodid === ci.prodid)
+        //Skrive ut HTML
+        cartHTML += `<tr>
+                        <td class="title">${product.title}</td>
+                        <td class="price">${product.price}</td>
+                        <td class="quantity">${ci.quantity}</td>
+                        <td class="delete"><button onClick = "deleteFromCart(${product.prodid})">X</button></td>
+                    </tr>`
+
+        //Summere totalpris
+        totalPrice += Number(product.price) * ci.quantity
+
+    })
+
+    if(cart.length === 0) {
+        cartHTML += "<tr><td>Ingen varer i handlevognen</td></tr>"
+    }
+
+    document.getElementById("cart-items").innerHTML = cartHTML
+    document.getElementById("total-price").innerHTML = totalPrice
+    document.getElementById("cart-quantity").innerHTML = cart.length
+
+}
+
+
+//Slette fra handlevogn
+function deleteFromCart(prodid) {
+    let deleteIndex = cart.indexOf(prodid)
+    if(deleteIndex > -1) {
+        cart.splice(deleteIndex, 1)
+    }
+
+    //Oppdatere handlevogn-oppskrift
+    showCart()
+
+}
+
+
+
 // legg til i handlevogn
 
 function addToCart(prodid) {
@@ -29,4 +88,8 @@ console.log("Legg til produkt med id: " + prodid)
 cart.push(prodid)
 console.log(cart)
 document.getElementById("cart-quantity").innerHTML = cart.length
+
+//Oppdater handlevisning:
+showCart()
+
 }
